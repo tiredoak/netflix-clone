a38acdbc80b42f466d4caa21ef374e06b0eeaf17
Miguel Carvalho <miguelc@tuta.io>
2022-04-08 14:06:01 +0100

    Banner styling + truncating movie description in banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -20,6 +21,10 @@ const Banner = () => {
 
   console.log(movie);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  }
+
   return (
     <header
       className='banner'
@@ -29,12 +34,16 @@ const Banner = () => {
         backgroundPosition: 'center center',
       }}>
       <div className='banner__contents'>
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className='banner__title'>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className='banner__buttons'>
           <button className='banner__button'>Play</button>
           <button className='banner__button'>My List</button>
         </div>
-        <h1 className='banner__description'>{movie.overview}</h1>
+        <h1 className='banner__description'>
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
     </header>
   );
