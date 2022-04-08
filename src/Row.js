d6c8b108a61131ca8889d22825c962d7e30b1036
Miguel Carvalho <miguelc@tuta.io>
2022-04-08 10:22:09 +0100

    Rendering unstyled movie posters

--- src/Row.js
+++ src/Row.js
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
+const BASE_URL = 'https://image.tmdb.org./t/p/original/';
+
 const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+
+  console.table(movies);
 
   return (
-    <div>
+    <div className='row'>
       <h2>{title}</h2>
+      <div className='row__posters'>
+        {/* several row___posters */}
+        {movies.map((movie) => {
+          return (
+            <img
+              className='row__poster'
+              src={`${BASE_URL}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
