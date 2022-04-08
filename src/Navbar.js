7f46d5a212c05bf03d32d3e5fa1303b4ef209856
Miguel Carvalho <miguelc@tuta.io>
2022-04-08 14:55:47 +0100

    Add transition effect in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 
 function Navbar() {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
   return (
-    <div className='navbar'>
+    <div className={`navbar ${show && 'navbar__black'}`}>
       <img
         className='navbar__logo'
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158'
