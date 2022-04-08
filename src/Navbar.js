89de4b1dede8c4e4bd80716c6704d2c1cce76771
Miguel Carvalho <miguelc@tuta.io>
2022-04-08 14:44:00 +0100

    Navbar styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className='nav'>
+    <div className='navbar'>
       <img
-        className='nav__logo'
+        className='navbar__logo'
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158'
         alt='Netflix Logo'
       />
       <img
-        className='nav__avatar'
+        className='navbar__avatar'
         src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
         alt='Netflix Avatar'
       />
