c36bad100347fb151a3bd07ab8fce61c2a5dea39
Miguel Carvalho <miguelc@tuta.io>
2022-04-08 14:35:40 +0100

    JSX for Navbar, rendering logo and avatar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
+import Navbar from './Navbar';
 
 const App = () => {
   return (
     <div className='app'>
-      {/* Navbar */}
+      <Navbar />
       <Banner />
       <Row
         title='Netflix Originals'
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className='nav'>
+      <img
+        className='nav__logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158'
+        alt='Netflix Logo'
+      />
+      <img
+        className='nav__avatar'
+        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+        alt='Netflix Avatar'
+      />
+    </div>
+  );
+}
+
+export default Navbar;
