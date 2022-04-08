f72a3cacf7c054b0c8a4b2541256deaec28a3d33
Miguel Carvalho <miguelc@tuta.io>
2022-04-08 11:53:06 +0100

    Removed App CSS and added all rows

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import requests from './requests';
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
       <Row
         title='Netflix Originals'
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
       <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentariesMovies} />
     </div>
   );
 };
